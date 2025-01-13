bba731c416e0789306e94eb5fb0c4df0e0b26746
Mounish <mounishnanu@gmail.com>
2025-01-12 23:48:38 -0500

    Added UUID

--- productService/src/main/java/com/ecommerce/productservice/models/Category.java
+++ productService/src/main/java/com/ecommerce/productservice/models/Category.java
@@ -1,5 +1,8 @@
 package com.ecommerce.productservice.models;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Category extends BaseModel{
     private String name;
 }
--- productService/src/main/java/com/ecommerce/productservice/models/Product.java
+++ productService/src/main/java/com/ecommerce/productservice/models/Product.java
@@ -1,13 +1,17 @@
 package com.ecommerce.productservice.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel{
     private String title;
     private String description;
     private String image;
+    @ManyToOne
     private Category category;
     private double price;
 }
